5f1289b86119ec7157fcb7b146a19847156623a5
nedodn <odennate@gmail.com>
2017-12-08 16:17:15 -0600

    new everything
    changed a whole bunch of stuff

---
+++ app/javascripts/app.js
@@ -0,0 +1,133 @@
+// Import the page's CSS. Webpack will know what to do with it.
+import "../stylesheets/app.css";
+
+// Import libraries we need.
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
+
+// Import our contract artifacts and turn them into usable abstractions.
+import opus_artifacts from '../../build/contracts/Opus.json'
+
+// MetaCoin is our usable abstraction, which we'll use through the code below.
+var Opus = contract(opus_artifacts);
+
+// The following code is simple to show off interacting with your contracts.
+// As your needs grow you will likely need to change its form and structure.
+// For application bootstrapping, check out window.addEventListener below.
+var accounts;
+var account;
+
+window.App = {
+  start: function() {
+    var self = this;
+
+    // Bootstrap the Opus abstraction for Use.
+    Opus.setProvider(web3.currentProvider);
+
+    // Get the initial account balance so it can be displayed.
+    web3.eth.getAccounts(function(err, accs) {
+      if (err != null) {
+        alert("There was an error fetching your accounts.");
+        return;
+      }
+
+      if (accs.length == 0) {
+        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+        return;
+      }
+
+      accounts = accs;
+      account = accounts[0];
+
+      self.buildTable();
+      self.getComp();
+    });
+  },
+
+  setStatus: function(message) {
+    var status = document.getElementById("status");
+    status.innerHTML = message;
+  },
+
+  buildTable: function() {
+    var self = this;
+    
+    $("#tableLabel").html("");
+    
+    $("#tableLabel").append("<th>Pitch</th>");
+    for(let i = 0; i < 1000; i++) {
+      $("#tableLabel").append("<th>Note: " + (i + 1) + " </th>");
+    }
+    
+    for(let i = 0; i < 128; i++) {
+      $("#comp").append("<tr><td> " + (128 - i) + "</td>");
+      for(let x = 0; x < 1000; x++) {
+        $("comp").append("<td id=' " + i.toString() + "note" + x.toString() + "'>    </td>");
+      }
+    }
+  },
+
+  getComp: function() {
+    var self = this;
+
+    Opus.deployed().then(function(instance) {
+      return instance.getComposition.call({from: account});
+    }).then(function(comp) {
+      for(let i = 0; i < 1000; i++) {
+
+      }
+  }
+},
+
+  refreshBalance: function() {
+    var self = this;
+
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.getBalance.call(account, {from: account});
+    }).then(function(value) {
+      var balance_element = document.getElementById("balance");
+      balance_element.innerHTML = value.valueOf();
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error getting balance; see log.");
+    });
+  },
+
+  sendCoin: function() {
+    var self = this;
+
+    var amount = parseInt(document.getElementById("amount").value);
+    var receiver = document.getElementById("receiver").value;
+
+    this.setStatus("Initiating transaction... (please wait)");
+
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.sendCoin(receiver, amount, {from: account});
+    }).then(function() {
+      self.setStatus("Transaction complete!");
+      self.refreshBalance();
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error sending coin; see log.");
+    });
+  }
+};
+
+window.addEventListener('load', function() {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+  }
+
+  App.start();
+});
--- contracts/Opus.sol
+++ contracts/Opus.sol
@@ -2,154 +2,109 @@ pragma solidity ^0.4.17;
 
 contract Opus {
 
-    //Note structure consisting of four pitches (Midi values from 0-128, 128 will represent a rest ), 
-    //duration (value between 1 and 8: 1 = sixteenth, 2 = eight, 3 = dotted eigth, 4 = quarter, 
-    //5 = dotted quarter, 6 = half, 7 = dotted half, 8 = whole,
-    // the owner of the note, and price set by owner
-    struct note {
-        uint[4] pitches;
-        uint duration;
-        address composer;
-        bool forSale;
-        uint price;
+    struct noteId {
+        uint pitch;
+        uint place;
+        address placer;
     }
 
-    address owner;
+    bool[129][100] composition;
 
-    //The composition will consist of a fixed number of notes, number may be subject to change
-    note[1000] composition;
-    mapping (address => uint[]) ownedNoteIds;
+    uint notes = 5000;
+    uint notesAvailable;
+    uint notePrice;
+
+    address owner;
+    uint endTime;
+
+    mapping (address => uint) ownedNotes;
+    noteId[] placedNotes;
 
     uint durationLockTime;
     uint pitchLockTime;
     bool composingStarted;
 
-    modifier noteForSale(uint _id) {
-        require(composition[_id].forSale);
+    modifier beforeEndTime() {
+        require(now < endTime);
         _;
     }
 
-    modifier onlyComposer(uint _id) {
-        require(composition[_id].composer == msg.sender);
+    modifier isValidNote(uint _pitch, uint _place) {
+        require(_pitch >= 0 && _pitch < 129);
+        require(_place >= 0 && _place < 100);
         _;
     }
 
-    modifier beforeDurationLock() {
-        require(now < durationLockTime);
-        _;
-    }
-
-    modifier beforePitchLock() {
-        require(now < pitchLockTime);
-        _;
-    }
-
-    //Constructor that gives all notes to sender, sets them for sale, and sets their price to an initial value
-    function Opus(uint _durationLockTime, uint _pitchLockTime) {
-
-        require(_durationLockTime > now && _pitchLockTime > _durationLockTime);
-
+    function Opus(uint _endTime) {
         owner = msg.sender;
 
-        durationLockTime = _durationLockTime;
-        pitchLockTime = _pitchLockTime;
-        composingStarted = false;
+        endTime = _endTime;
+
+        notesAvailable = 5000;
+        notePrice = 0.01 ether;
     }
 
-    function startComposing() external {
-        require(owner == msg.sender && !composingStarted);
+    function purchaseNote(uint numNotes) beforeEndTime() external payable {
+        require(numNotes < 100 && numNotes <= notesAvailable && msg.value == (numNotes * notePrice));
 
-        for (uint i = 0; i < 1000; i++) {
-            composition[i].composer = msg.sender;
-            composition[i].forSale = true;
-            composition[i].price = 0.01 ether;
+        if (ownedNotes[msg.sender] == 0) {
+            ownedNotes[msg.sender] = numNotes;
+        }else {
+            ownedNotes[msg.sender] = ownedNotes[msg.sender] + numNotes;
+        }
+
+        notesAvailable = notesAvailable - numNotes;
+    }
+
+    function placeNote(uint _pitch, uint _place) beforeEndTime() isValidNote(_pitch, _place) external {
+        require(ownedNotes[msg.sender] > 0 && !composition[_pitch][_place]);
+
+        noteId memory note;
+        note.pitch = _pitch;
+        note.place = _place;
+        note.placer = msg.sender;
+
+        placedNotes.push(note);
+
+        composition[_pitch][_place] = true;
+        ownedNotes[msg.sender]--;
+    }
+
+    function removeNote(uint _pitch, uint _place) beforeEndTime() isValidNote(_pitch, _place) external {
+        require(composition[_pitch][_place] && noteOwner(_pitch, _place, msg.sender));
+
+        composition[_pitch][_place] = false;
+
+        uint noteIndex = getPlacedNoteIndex(_pitch, _place, msg.sender);
+
+        delete placedNotes[noteIndex];
+        ownedNotes[msg.sender]++;
+    }
+
+    function noteOwner(uint _pitch, uint _place, address sender) internal view returns (bool) {
+        for (uint i = 0; i < placedNotes.length; i++) {
+            noteId memory note = placedNotes[i];
+            if (note.pitch == _pitch && note.place == _place && note.placer == sender) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function getPlacedNoteIndex(uint _pitch, uint _place, address sender) internal view returns (uint) {
+        for (uint i = 0; i < placedNotes.length; i++) {
+            noteId memory note = placedNotes[i];
+            if (note.pitch == _pitch && note.place == _place && note.placer == sender) {
+                return i;
+            }
         }
     }
 
-    //lets owner of a note change the pitches, after a certain amount of time pitches will be locked, end of composition 
-    function setNotePitches(uint _id, uint[4] _pitches) onlyComposer(_id) beforePitchLock() external {
-
-        for (uint i = 0; i < 4; i++) {
-            composition[_id].pitches[i] = _pitches[i];
-        }
-
+    function getComposition() external view returns (bool[129][100] _composition) {
+        bool[129][100] memory temp = composition;
+        return temp;
     }
 
 
-    //lets owner of note change the duration, after a certain amount of time durations will be locked
-    function setNoteDuration(uint _id, uint _duration) onlyComposer(_id) beforeDurationLock() external {
-
-        require(_duration >= 1 && _duration <= 8);
-
-        composition[_id].duration = _duration;
-
-    }
-
-    //lets someone purchase a note that is listed for sale, give it a new price. 
-    function purchaseNote(uint _id, uint _price) noteForSale(_id) beforePitchLock() external payable {
-        require(composition[_id].composer != msg.sender);
-
-        uint price = composition[_id].price;
-        require(msg.value == price);
-
-        composition[_id].price = 0;
-
-        composition[_id].composer.transfer(price);
-
-        composition[_id].composer == msg.sender;
-        composition[_id].price = _price;
-        composition[_id].forSale = false;
-
-        ownedNoteIds[msg.sender].push(_id);
-    }
-
-    //set owned note for sale or not for sale
-    function toggleForSale(uint _id) onlyComposer(_id) beforePitchLock() external {
-        composition[_id].forSale = !composition[_id].forSale;
-    }
-
-    //set an owned notes price
-    function setNotePrice(uint _id, uint _price) onlyComposer(_id) beforePitchLock() external {
-        composition[_id].price = _price;
-    }
-
-    //get any notes owned by the sender
-    function getOwnedNotes() external view returns (uint[]) {
-
-        uint[] memory _ownedNotes = ownedNoteIds[msg.sender];
-
-        return _ownedNotes;
-    }
-
-    //get specific note information
-    function getNoteInformation(uint _id) external view returns (uint[4] _pitches,
-                                                                uint _duration,
-                                                                address _composer,
-                                                                bool _forSale,
-                                                                uint price
-                                                                ) {
-        uint[4] memory pitches = composition[_id].pitches;
-        _pitches = pitches;
-        _duration = composition[_id].duration;
-        _composer = composition[_id].composer;
-        _forSale = composition[_id].forSale;
-        price = composition[_id].price;
-    }
-
-    //get whole composition
-    function getComposition() external view returns (uint[4][1000] _pitches,
-                                                     uint[1000] _durations,
-                                                     address[1000] _composers,
-                                                     bool[1000] _forSales,
-                                                     uint[1000] _prices
-                                                    ) {
-        for (uint i = 0; i < 1000; i++) {
-            _pitches[i] = composition[i].pitches;
-
-            _durations[i] = composition[i].duration;
-            _composers[i] = composition[i].composer;
-            _forSales[i] = composition[i].forSale;
-            _prices[i] = composition[i].price;
-        }                                                                                               
-    }
 }
