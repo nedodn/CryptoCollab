628436ba81bc514c0a6834d2748c93c834091bd9
nedodn <odennate@gmail.com>
2017-12-12 21:15:31 -0600

    added better notes

---
+++ app/javascripts/notes.js
@@ -0,0 +1,528 @@
+function getNoteName(note) {
+    let name, color;
+
+    switch(note) {
+        case 127:
+            name = "C0";
+            color = true;
+            break;
+        case 126:
+            name = "C#0/Db0";
+            color = false;
+            break;
+        case 125:
+            name = "D0";
+            color = true;
+            break;
+        case 124:
+            name = "D#0/Eb0";
+            color = false;
+            break;
+        case 123:
+            name = "E0";
+            color = true;
+            break;
+        case 122:
+            name = "F0";
+            color = true;
+            break;
+        case 121:
+            name = "F#0/Gb0"
+            color = false;
+            break;
+        case 120:
+            name = "G0";
+            color = true;
+            break;
+        case 119:
+            name = "G#0/Ab0";
+            color = false;
+            break;
+        case 118:
+            name = "A0";
+            color = true;
+            break;
+        case 117:
+            name = "A#0/Bb0";
+            color = false;
+            break;
+        case 116:
+            name = "B0";
+            color = true;
+            break;
+        case 115:
+            name = "C1";
+            color = true;
+            break;
+        case 114:
+            name = "C#1/Db1";
+            color = false;
+            break;
+        case 113:
+            name = "D1";
+            color = true;
+            break;
+        case 112:
+            name = "D#1/Eb1";
+            color = false;
+            break;
+        case 111:
+            name = "E1";
+            color = true;
+            break;
+        case 110:
+            name = "F1";
+            color = true;
+            break;
+        case 109:
+            name = "F#1/Gb1"
+            color = false;
+            break;
+        case 108:
+            name = "G1";
+            color = true;
+            break;
+        case 107:
+            name = "G#1/Ab1";
+            color = false;
+            break;
+        case 106:
+            name = "A1";
+            color = true;
+            break;
+        case 105:
+            name = "A#1/Bb1";
+            color = false;
+            break;
+        case 104:
+            name = "B1";
+            color = true;
+            break;
+        case 103:
+            name = "C2";
+            color = true;
+            break;
+        case 102:
+            name = "C#2/Db2";
+            color = false;
+            break;
+        case 101:
+            name = "D2";
+            color = true;
+            break;
+        case 100:
+            name = "D#2/Eb2";
+            color = false;
+            break;
+        case 99:
+            name = "E2";
+            color = true;
+            break;
+        case 98:
+            name = "F2";
+            color = true;
+            break;
+        case 97:
+            name = "F#2/Gb2"
+            color = false;
+            break;
+        case 96:
+            name = "G2";
+            color = true;
+            break;
+        case 95:
+            name = "G#2/Ab2";
+            color = false;
+            break;
+        case 94:
+            name = "A2";
+            color = true;
+            break;
+        case 93:
+            name = "A#2/Bb2";
+            color = false;
+            break;
+        case 92:
+            name = "B2";
+            color = true;
+            break;
+        case 91:
+            name = "C3";
+            color = true;
+            break;
+        case 90:
+            name = "C#3/Db3";
+            color = false;
+            break;
+        case 89:
+            name = "D3";
+            color = true;
+            break;
+        case 88:
+            name = "D#3/Eb3";
+            color = false;
+            break;
+        case 87:
+            name = "E3";
+            color = true;
+            break;
+        case 86:
+            name = "F3";
+            color = true;
+            break;
+        case 85:
+            name = "F#3/Gb3"
+            color = false;
+            break;
+        case 84:
+            name = "G3";
+            color = true;
+            break;
+        case 83:
+            name = "G#3/Ab3";
+            color = false;
+            break;
+        case 82:
+            name = "A3";
+            color = true;
+            break;
+        case 81:
+            name = "A#3/Bb3";
+            color = false;
+            break;
+        case 80:
+            name = "B3";
+            color = true;
+            break;
+        case 79:
+            name = "C4";
+            color = true;
+            break;
+        case 78:
+            name = "C#4/Db4";
+            color = false;
+            break;
+        case 77:
+            name = "D4";
+            color = true;
+            break;
+        case 76:
+            name = "D#4/Eb4";
+            color = false;
+            break;
+        case 75:
+            name = "E4";
+            color = true;
+            break;
+        case 74:
+            name = "F4";
+            color = true;
+            break;
+        case 73:
+            name = "F#4/Gb4"
+            color = false;
+            break;
+        case 72:
+            name = "G4";
+            color = true;
+            break;
+        case 71:
+            name = "G#4/Ab4";
+            color = false;
+            break;
+        case 70:
+            name = "A4";
+            color = true;
+            break;
+        case 69:
+            name = "A#4/Bb4";
+            color = false;
+            break;
+        case 68:
+            name = "B4";
+            color = true;
+            break;
+        case 67:
+            name = "C5";
+            color = true;
+            break;
+        case 66:
+            name = "C#5/Db5";
+            color = false;
+            break;
+        case 65:
+            name = "D5";
+            color = true;
+            break;
+        case 64:
+            name = "D#5/Eb5";
+            color = false;
+            break;
+        case 63:
+            name = "E5";
+            color = true;
+            break;
+        case 62:
+            name = "F5";
+            color = true;
+            break;
+        case 61:
+            name = "F#5/Gb5"
+            color = false;
+            break;
+        case 60:
+            name = "G5";
+            color = true;
+            break;
+        case 59:
+            name = "G#5/Ab5";
+            color = false;
+            break;
+        case 58:
+            name = "A5";
+            color = true;
+            break;
+        case 57:
+            name = "A#5/Bb5";
+            color = false;
+            break;
+        case 56:
+            name = "B5";
+            color = true;
+            break;
+        case 55:
+            name = "C6";
+            color = true;
+            break;
+        case 54:
+            name = "C#6/Db6";
+            color = false;
+            break;
+        case 53:
+            name = "D6";
+            color = true;
+            break;
+        case 52:
+            name = "D#6/Eb6";
+            color = false;
+            break;
+        case 51:
+            name = "E6";
+            color = true;
+            break;
+        case 50:
+            name = "F6";
+            color = true;
+            break;
+        case 49:
+            name = "F#6/Gb6"
+            color = false;
+            break;
+        case 48:
+            name = "G6";
+            color = true;
+            break;
+        case 47:
+            name = "G#6/Ab6";
+            color = false;
+            break;
+        case 46:
+            name = "A6";
+            color = true;
+            break;
+        case 45:
+            name = "A#6/Bb6";
+            color = false;
+            break;
+        case 44:
+            name = "B6";
+            color = true;
+            break;
+        case 43:
+            name = "C7";
+            color = true;
+            break;
+        case 42:
+            name = "C#7/Db7";
+            color = false;
+            break;
+        case 41:
+            name = "D7";
+            color = true;
+            break;
+        case 40:
+            name = "D#7/Eb7";
+            color = false;
+            break;
+        case 39:
+            name = "E7";
+            color = true;
+            break;
+        case 38:
+            name = "F7";
+            color = true;
+            break;
+        case 37:
+            name = "F#7/Gb7"
+            color = false;
+            break;
+        case 36:
+            name = "G7";
+            color = true;
+            break;
+        case 35:
+            name = "G#7/Ab7";
+            color = false;
+            break;
+        case 34:
+            name = "A7";
+            color = true;
+            break;
+        case 33:
+            name = "A#7/Bb7";
+            color = false;
+            break;
+        case 32:
+            name = "B7";
+            color = true;
+            break;
+        case 31:
+            name = "C8";
+            color = true;
+            break;
+        case 30:
+            name = "C#8/Db8";
+            color = false;
+            break;
+        case 29:
+            name = "D8";
+            color = true;
+            break;
+        case 28:
+            name = "D#8/Eb8";
+            color = false;
+            break;
+        case 27:
+            name = "E8";
+            color = true;
+            break;
+        case 26:
+            name = "F8";
+            color = true;
+            break;
+        case 25:
+            name = "F#8/Gb8"
+            color = false;
+            break;
+        case 24:
+            name = "G8";
+            color = true;
+            break;
+        case 23:
+            name = "G#8/Ab8";
+            color = false;
+            break;
+        case 22:
+            name = "A8";
+            color = true;
+            break;
+        case 21:
+            name = "A#8/Bb8";
+            color = false;
+            break;
+        case 20:
+            name = "B8";
+            color = true;
+            break;
+        case 19:
+            name = "C9";
+            color = true;
+            break;
+        case 18:
+            name = "C#9/Db9";
+            color = false;
+            break;
+        case 17:
+            name = "D9";
+            color = true;
+            break;
+        case 16:
+            name = "D#9/Eb9";
+            color = false;
+            break;
+        case 15:
+            name = "E9";
+            color = true;
+            break;
+        case 14:
+            name = "F9";
+            color = true;
+            break;
+        case 13:
+            name = "F#9/Gb9"
+            color = false;
+            break;
+        case 12:
+            name = "G9";
+            color = true;
+            break;
+        case 11:
+            name = "G#9/Ab9";
+            color = false;
+            break;
+        case 10:
+            name = "A9";
+            color = true;
+            break;
+        case 9:
+            name = "A#9/Bb9";
+            color = false;
+            break;
+        case 8:
+            name = "B9";
+            color = true;
+            break;
+        case 7:
+            name = "C10";
+            color = true;
+            break;
+        case 6:
+            name = "C#10/Db10";
+            color = false;
+            break;
+        case 5:
+            name = "D10";
+            color = true;
+            break;
+        case 4:
+            name = "D#10/Eb10";
+            color = false;
+            break;
+        case 3:
+            name = "E10";
+            color = true;
+            break;
+        case 2:
+            name = "F10";
+            color = true;
+            break;
+        case 1:
+            name = "F#10/Gb10"
+            color = false;
+            break;
+        case 0:
+            name = "G10";
+            color = true;
+            break;
+    }
+
+    let noteName =  {
+        name: name,
+        color: color
+    }
+
+    return noteName
+
+}
+
+export {getNoteName};
