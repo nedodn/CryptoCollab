1683206b526111d314d38252b62e632284a814a0
chuckfairy <charlieabeling@gmail.com>
2018-04-23 18:24:56 -0500

    DS store removal, ui cleanup, address and network header, lock

---
+++ app/javascripts/networks.js
@@ -0,0 +1,20 @@
+/**
+ * Eth networks data
+ */
+"use strict";
+
+var Networks = {
+
+    "1": "mainnet",
+
+    "2": "morden",
+
+    "3": "ropsten",
+
+    "4": "rinkeby",
+
+    "42": "kovan"
+
+};
+
+export { Networks };
